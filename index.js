b5dedf8bb569d8bb209614c83ee995a3c5b6f5aa
Vatsalpanara <159544008+Vatsalpanara@users.noreply.github.com>
2024-12-26 13:58:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require("express")
 const port = 3001;
 const path = require("path");
+const fs = require("fs");
+const cookies = require("cookie-parser");
 const db = require("./config/db")
 
 const app = express();
+const passport = require("./middleware/passport");
+const session = require("express-session");
 
 app.set("view engine","ejs");
 app.use(express.urlencoded());
 app.use("/",require("./routes/route"))
+app.use(cookies());
+
+app.use(
+    session({
+        name:"local",
+        secret:"local",
+        resave:true,
+        saveUninitialized:false,
+        cookie:{maxAge:100*100*60,httpOnly:true},
+    })
+);
+
+app.use(passport.initalize());
+app.use(passport.session());
+
 app.use("/public",express.static(path.join(__dirname,"public")))
 app.use("/uploads",express.static(path.join(__dirname,"uploads")))
 
